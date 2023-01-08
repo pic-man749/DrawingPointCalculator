aea1420897b348c8de77478ed7545b79b6933f42
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2023-01-08 10:57:29 +0900

    bugfix, and add wrapper

--- DrawingPointCalculator.ps1
+++ DrawingPointCalculator.ps1
@@ -1,12 +1,14 @@
 class DrawingPointCalculator{
 
-    [int] $x = 0;
-    [int] $y = 0;
-    [int] $reservedX = 0;
-    [int] $reservedY = 0;
+    [int] $x = 0
+    [int] $y = 0
+    [int] $reservedX = 0
+    [int] $reservedY = 0
 
-    [int] $marginX = 0;
-    [int] $marginY = 0;
+    [int] $marginX = 0
+    [int] $marginY = 0
+
+    [array] $wrapperY = @()
 
     DrawingPointCalculator([int] $marginX, [int] $marginY){
         $this.marginX = $marginX
@@ -27,22 +29,22 @@ class DrawingPointCalculator{
 
     [void] addObjV([int] $objW, [int] $objH){
 
-        if($this.reservedX < ($this.x + $objW + $marginX)){
-            $this.reservedX = $this.x + $objW + $marginX
+        if($this.reservedX -lt ($this.x + $objW + $this.marginX)){
+            $this.reservedX = $this.x + $objW + $this.marginX
         }
-        if($this.reservedY < ($this.y + $objH + $marginY)){
-            $this.reservedY = $this.y + $objH + $marginY
+        if($this.reservedY -lt ($this.y + $objH + $this.marginY)){
+            $this.reservedY = $this.y + $objH + $this.marginY
         }
         # $this.x = $this.x
         $this.y = $this.y + $objH + $this.marginY
     }
 
     [void] addObjH([int] $objW, [int] $objH){
-        if($this.reservedX < ($this.x + $objW + $marginX)){
-            $this.reservedX = $this.x + $objW + $marginX
+        if($this.reservedX -lt ($this.x + $objW + $this.marginX)){
+            $this.reservedX = $this.x + $objW + $this.marginX
         }
-        if($this.reservedY < ($this.y + $objH + $marginY)){
-            $this.reservedY = $this.y + $objH + $marginY
+        if($this.reservedY -lt ($this.y + $objH + $this.marginY)){
+            $this.reservedY = $this.y + $objH + $this.marginY
         }
         $this.x = $this.x + $objW + $this.marginX
         # $this.y = $this.y
@@ -51,5 +53,19 @@ class DrawingPointCalculator{
     [void] newLine(){
         $this.x = $this.marginX
         $this.y = $this.reservedY
+    } 
+
+    [void] beginWrapper(){
+        $this.wrapperY += $this.y
+    }
+
+    [void] endWrapper(){
+        [int] $len = $this.wrapperY.Length
+        if($len -eq 0){
+            throw "there is no Wrapper"
+        }
+        $this.x = $this.reservedX
+        $this.y = $this.wrapperY[$len - 1]
+        $this.wrapperY[0..($len - 2)]
     }
 }
